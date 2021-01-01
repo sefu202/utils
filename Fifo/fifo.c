b0c8800f40763b5858abe769a9e6b7fa979db74a
sefu202 <49610245+sefu202@users.noreply.github.com>
2021-01-01 16:23:35 +0100

    Update fifo.c

--- Fifo/fifo.c
+++ Fifo/fifo.c
@@ -1,20 +1,13 @@
 // *** INCLUDES ***
 #include "fifo.h"
 #include <string.h> // memcpy 
-#ifdef _DEBUG
-    #include <assert.h>
-#endif
+#include <assert.h>
 #if FIFO_ALLOW_MALLOC == true
     #include <malloc.h>
 #endif /* FIFO_ALLOW_MALLOC */
 
-// *** DEFINES ***
-#define _WRITE_LOCK 0x01
-#define _READ_LOCK  0x02
-
 /**
  * @brief initializes a fifo handle
- * @note If _DEBUG is defined every Parameter will be checked with assert()
  * @param pHandle pointer to the fifo handle
  * @param pFifo pointer to the fifo memory
  * @param size_fifo size of the fifo in bytes, must be a multiple of bastype size
@@ -27,13 +20,11 @@
  */
 int8_t fifo_init(volatile fifo_handle_t *pHandle, void *pFifo, FIFO_INDEX_TYPE size_fifo, SIZE_FIFO_BASE_TYPE basetype_size)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
     assert(pFifo != NULL);
     assert(size_fifo <= MAX_FIFO_SIZE && size_fifo > 0);
     assert(basetype_size > 0 && basetype_size <= FIFO_MAX_BASETYPE_SIZE);
     assert(size_fifo % basetype_size == 0);
-#endif
 
     // *** Checking Parameters ***
     if (pHandle == NULL || pFifo == NULL)
@@ -51,14 +42,12 @@ int8_t fifo_init(volatile fifo_handle_t *pHandle, void *pFifo, FIFO_INDEX_TYPE s
     pHandle->pFifo = pFifo;
     pHandle->read_idx = 0;
     pHandle->write_idx = 0;
-    pHandle->_lock = 0;
     return 0;
 }
 
 #if FIFO_ALLOW_MALLOC
 /**
  * @brief allocates a fifo handle and the fifo memory, and initializes it
- * @note If _DEBUG is defined every Parameter will be checked with assert()
  * @note memory has to be freed with fifo_deinit_free()
  * @param size_fifo size of the fifo in elements
  * @param basetype_size size of the fifo basetype eg: sizeof(uint8_t)
@@ -67,10 +56,8 @@ int8_t fifo_init(volatile fifo_handle_t *pHandle, void *pFifo, FIFO_INDEX_TYPE s
  */
 fifo_handle_t* fifo_init_malloc(FIFO_INDEX_TYPE size_fifo, SIZE_FIFO_BASE_TYPE basetype_size)
 {
- #ifdef _DEBUG
     assert(size_fifo <= MAX_FIFO_SIZE && size_fifo > 0);
     assert(basetype_size > 0 && basetype_size <= FIFO_MAX_BASETYPE_SIZE);
-#endif
     // *** Checking Parameters ***
     if (size_fifo > MAX_FIFO_SIZE || size_fifo == 0)
         return NULL;
@@ -93,7 +80,6 @@ fifo_handle_t* fifo_init_malloc(FIFO_INDEX_TYPE size_fifo, SIZE_FIFO_BASE_TYPE b
             myHandle->basetype_size = basetype_size;
             myHandle->read_idx = 0;
             myHandle->write_idx = 0;
-            myHandle->_lock = 0;
         }
         else     // buffer allocation failed
         {
@@ -106,14 +92,11 @@ fifo_handle_t* fifo_init_malloc(FIFO_INDEX_TYPE size_fifo, SIZE_FIFO_BASE_TYPE b
 
 /**
  * @brief deallocates a fifo handle and its buffer
- * @note if _DEBUG is defined pHandle gets checked with assert()
  * @param pHandle pointer to the Fifo handle
  */
 void fifo_deinit_free(volatile fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
     if (pHandle->pFifo != NULL)
     {
         free(pHandle->pFifo);
@@ -127,7 +110,6 @@ void fifo_deinit_free(volatile fifo_handle_t *pHandle)
 
 /**
  * @brief puts an element into the fifo.
- * @note If _DEBUG is defined every Parameter will be checked with assert()
  * @param pHandle pointer to the fifo handle
  * @param [in] pData pointer to the data to be put onto the fifo
  * @return fifoerror_t
@@ -135,62 +117,49 @@ void fifo_deinit_free(volatile fifo_handle_t *pHandle)
 fifoerror_t fifo_put(volatile fifo_handle_t *pHandle, const void *pData)
 {
     fifoerror_t ret = FIFO_BUISY;
-#ifdef _DEBUG
+
     assert(pHandle != NULL);
     assert(pData != NULL);
-#endif
+
     // *** Checking Parameters ***
     if (pHandle == NULL)
         return FIFO_WRONG_PARAM;
     if (pData == NULL)
         return FIFO_WRONG_PARAM;
 
+    // *** Ring ***
+    FIFO_INDEX_TYPE idx_temp = pHandle->write_idx + pHandle->basetype_size, read_idx = pHandle->read_idx;
 
-    if (!(pHandle->_lock & _WRITE_LOCK))   // fifo is write-locked
+    if (idx_temp >= pHandle->size)
     {
-    FIFO_ENTER_CRITICAL();  // writing to lock may not be a atomic operation
-        pHandle->_lock |= _WRITE_LOCK;      // lock the handle
-
-        // *** Ring ***
-        FIFO_INDEX_TYPE idx_temp = pHandle->write_idx + pHandle->basetype_size, read_idx = pHandle->read_idx;
-    FIFO_LEAVE_CRITICAL();
-
-        if (idx_temp >= pHandle->size)
-        {
-            idx_temp = 0;
-        }
-
-        // *** Check if space available ***
-        if (idx_temp == read_idx)  // No space
-        {
-            ret = FIFO_FULL;
-        }
-        else        // space available
-        {
-            // *** Write to the fifo ***
-            memcpy(((uint8_t *)(pHandle->pFifo) + (pHandle->write_idx = idx_temp)), pData, pHandle->basetype_size);
-            ret = FIFO_NO_ERROR;
-        }
-    FIFO_ENTER_CRITICAL();
-        pHandle->_lock &= ~_WRITE_LOCK;     // unlock the handle
-    FIFO_LEAVE_CRITICAL();
+        idx_temp = 0;
     }
+
+    // *** Check if space available ***
+    if (idx_temp == read_idx)  // No space
+    {
+        ret = FIFO_FULL;
+    }
+    else        // space available
+    {
+        // *** Write to the fifo ***
+        memcpy(((uint8_t *)(pHandle->pFifo) + (pHandle->write_idx = idx_temp)), pData, pHandle->basetype_size);
+        ret = FIFO_NO_ERROR;
+    }
+
     return ret;
 }
 
 /**
  * @brief puts an element into the fifo.
- * @note If _DEBUG is defined every Parameter will be checked with assert()
  * @param pHandle pointer to the handle of the fifo
  * @param [out] pData pointer to the storage for the data from the fifo
  * @return fifoerror_t
  */
 fifoerror_t fifo_get(volatile fifo_handle_t *pHandle, void *pData)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
     assert(pData != NULL);
-#endif
     // *** Checking Parameters ***
     if (pHandle == NULL)
         return FIFO_WRONG_PARAM;
@@ -198,36 +167,26 @@ fifoerror_t fifo_get(volatile fifo_handle_t *pHandle, void *pData)
         return FIFO_WRONG_PARAM;
 
     FIFO_INDEX_TYPE idx_temp, write_idx;
-    fifoerror_t ret = FIFO_BUISY;
+    fifoerror_t ret = FIFO_NO_ERROR;
 
-    if (!(pHandle->_lock & _READ_LOCK))   // fifo is read-locked
+    write_idx = pHandle->write_idx;     // looking at write_idx may not be a atomic operation
+    // *** Check if data available ***
+    if (write_idx != pHandle->read_idx)  // no data in fifo
     {
-    FIFO_ENTER_CRITICAL();
-        pHandle->_lock |= _READ_LOCK;      // lock the handle
-        write_idx = pHandle->write_idx;     // looking at write_idx may not be a atomic operation
-    FIFO_LEAVE_CRITICAL();
-        // *** Check if data available ***
-        if (write_idx != pHandle->read_idx)  // no data in fifo
-        {
-            ret = FIFO_NO_ERROR;
-            // *** Ring ***
-            idx_temp = pHandle->read_idx + pHandle->basetype_size; 
+        // *** Ring ***
+        idx_temp = pHandle->read_idx + pHandle->basetype_size; 
 
-            if (idx_temp >= pHandle->size) 
-            {
-                idx_temp = 0;
-            }
-
-            // *** Copty the data ***
-            memcpy(pData, ((uint8_t *)(pHandle->pFifo)) + (pHandle->read_idx = idx_temp), pHandle->basetype_size);  
-        }
-        else
+        if (idx_temp >= pHandle->size) 
         {
-            ret = FIFO_EMPTY;
+            idx_temp = 0;
         }
-    FIFO_ENTER_CRITICAL();
-        pHandle->_lock &= ~_READ_LOCK;     // unlock the handle
-    FIFO_LEAVE_CRITICAL();
+
+        // *** Copty the data ***
+        memcpy(pData, ((uint8_t *)(pHandle->pFifo)) + (pHandle->read_idx = idx_temp), pHandle->basetype_size);  
+    }
+    else
+    {
+        ret = FIFO_EMPTY;
     }
 
     return ret;
@@ -241,17 +200,13 @@ fifoerror_t fifo_get(volatile fifo_handle_t *pHandle, void *pData)
  */
 bool fifo_hasElementsLeft(volatile fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
     bool ret = false;
     // *** Check if data available ***
-FIFO_ENTER_CRITICAL();
     if (!(pHandle == NULL || pHandle->write_idx == pHandle->read_idx))  // no data in fifo
     {
         ret = true;
     }
-FIFO_LEAVE_CRITICAL();
     return ret;
 }
 
@@ -262,14 +217,12 @@ FIFO_LEAVE_CRITICAL();
  */
 bool fifo_hasSpaceLeft(volatile fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
+
     assert(pHandle != NULL);
-#endif
     bool ret = false;
     // *** Checking Parameters ***
     if (pHandle != NULL)
-    {
-    FIFO_ENTER_CRITICAL();    
+    {   
         FIFO_INDEX_TYPE idx_temp = pHandle->write_idx + pHandle->basetype_size;
         if (idx_temp >= pHandle->size)
         {
@@ -281,7 +234,6 @@ bool fifo_hasSpaceLeft(volatile fifo_handle_t *pHandle)
         {
             ret = true;
         }
-    FIFO_LEAVE_CRITICAL();
     }
     return ret;
 }
@@ -289,26 +241,19 @@ bool fifo_hasSpaceLeft(volatile fifo_handle_t *pHandle)
 /**
  * @brief flushes a FIFO by setting write- and read-index to the same value
  * @return FIFO_NO_ERROR    Everything worked
- * @return FIFO_BUISY       FIFO handle is locked
  */
 fifoerror_t fifo_flush(volatile fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
+    fifoerror_t ret = FIFO_NO_ERROR;
     if (pHandle == NULL)
     {
-        return FIFO_WRONG_PARAM;
+        ret =  FIFO_WRONG_PARAM;
     }
-    fifoerror_t ret = FIFO_NO_ERROR;
-
-FIFO_ENTER_CRITICAL();
-    if (pHandle->_lock & (_READ_LOCK | _WRITE_LOCK))  // fifo is locked
+    else
     {
-        ret = FIFO_BUISY;
+        pHandle->read_idx = pHandle->write_idx; // flush
     }
-    pHandle->read_idx = pHandle->write_idx; // flush
-FIFO_LEAVE_CRITICAL();
     return ret;
 }
 
@@ -347,26 +292,16 @@ fifoerror_t fifo_skip_read(fifo_handle_t *pHandle)
  */
 fifoerror_t fifo_skip_read_n(fifo_handle_t *pHandle, FIFO_INDEX_TYPE n)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
     // *** Checking Parameters ***
     if (pHandle == NULL)
         return FIFO_WRONG_PARAM;
 
     FIFO_INDEX_TYPE idx_temp;
 
-FIFO_ENTER_CRITICAL();
-    if (pHandle->_lock & _READ_LOCK)   // fifo is read-locked
-    {
-        FIFO_LEAVE_CRITICAL();
-        return FIFO_BUISY;
-    }
-
     // *** Check if data available ***
     if (pHandle->write_idx == pHandle->read_idx)  // no data in fifo
     {
-        FIFO_LEAVE_CRITICAL();
         return FIFO_EMPTY;
     }
 
@@ -385,7 +320,6 @@ FIFO_ENTER_CRITICAL();
         idx_temp -= pHandle->size;
     }
     pHandle->read_idx = idx_temp;
-FIFO_LEAVE_CRITICAL();
 
     return FIFO_NO_ERROR;
 }
@@ -414,22 +348,13 @@ fifoerror_t fifo_skip_write(fifo_handle_t *pHandle)
  */
 fifoerror_t fifo_skip_write_n(fifo_handle_t *pHandle, FIFO_INDEX_TYPE n)
 {
-        fifoerror_t ret;
-#ifdef _DEBUG
+    fifoerror_t ret;
     assert(pHandle != NULL);
-#endif
     // *** Checking Parameters ***
     if (pHandle == NULL)
         return FIFO_WRONG_PARAM;
     if (pHandle == 0)
         return FIFO_WRONG_PARAM;
-
-FIFO_ENTER_CRITICAL();
-    if (pHandle->_lock & _WRITE_LOCK)   // fifo is write-locked
-    {
-        FIFO_LEAVE_CRITICAL();
-        return FIFO_BUISY;
-    }
 
     // *** Limit skipped elements to available elements ***
     FIFO_INDEX_TYPE space = fifo_getEmptySpace(pHandle);
@@ -456,7 +381,6 @@ FIFO_ENTER_CRITICAL();
         ret = FIFO_NO_ERROR;
         pHandle->write_idx = idx_temp;
     }
-FIFO_LEAVE_CRITICAL();
     return ret;
 }
 
@@ -467,17 +391,13 @@ FIFO_LEAVE_CRITICAL();
  */
 FIFO_INDEX_TYPE fifo_getLevel(fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
     if (pHandle == NULL)
     {
         return 0;
     }
-FIFO_ENTER_CRITICAL();
     FIFO_INDEX_TYPE ret = pHandle->write_idx, read_idx = pHandle->read_idx;
-FIFO_LEAVE_CRITICAL();
-    if (read_idx > ret) // ret alias write_idx
+    if (read_idx > ret) // ret aka write_idx
     {
         ret += (pHandle->size - read_idx);
     }
@@ -495,9 +415,7 @@ FIFO_LEAVE_CRITICAL();
  */
 FIFO_INDEX_TYPE fifo_getEmptySpace(fifo_handle_t *pHandle)
 {
-#ifdef _DEBUG
     assert(pHandle != NULL);
-#endif
     if (pHandle == NULL)
     {
         return 0;
